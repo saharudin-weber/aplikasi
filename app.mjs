f08c9a213204b447ff32220c589a3fc7b5dd7ca2
unknown <saharudinqwe@gmail.com>
2022-09-22 01:26:37 +0800

    make it better

--- app.mjs
+++ app.mjs
@@ -25,7 +25,7 @@ const __dirname = path.dirname(__filename)
 const router = Router()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port =process.env.PORT || 3000
 
 
 app.set('view engine','ejs')
